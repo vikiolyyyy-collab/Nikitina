3cdc20fdd927bfa20f92168806898b69e207b005
vikiolyyyy-collab <vikiolyyyy@gmail.com>
2025-09-21 13:19:54 +0300

    changed python fail -N

--- python.py
+++ python.py
@@ -2,3 +2,5 @@ print('hello world')
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
 print(f"Сумма чисел: {num1 + num2}")
+
+print(f"Разность чисел: {num1 - num2}")
